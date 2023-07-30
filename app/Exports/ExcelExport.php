b9b73c96e29c30621303c440e8832c91d8f53a6b
TajulMunandar <tajulmunandar701@gmail.com>
2023-07-30 20:43:00 +0700

    finish

--- app/Exports/ExcelExport.php
+++ app/Exports/ExcelExport.php
@@ -9,7 +9,6 @@ use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Style\Border;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
@@ -32,23 +31,20 @@ class ExcelExport implements FromCollection, WithHeadings, ShouldAutoSize, WithS
             $tanggal = $kegiatan->tanggal; // Mengambil tanggal dalam format 'Y-m-d'
             $tanggal = Carbon::parse($tanggal)->locale('id')->isoFormat('dddd, DD MMMM YYYY');
 
-            $hasil = $kegiatan->detailkegiatans->map(function ($detailKegiatan) {
-                return $detailKegiatan->hasil; // Mengambil data 'hasil' dari model DetailKegiatan
-            })->implode(', ');
-
-            $Kegiatans = $kegiatan->detailkegiatans->map(function ($detailKegiatan) {
-                return $detailKegiatan->kegiatan; // Mengambil data 'hasil' dari model DetailKegiatan
-            })->implode(', ');
-
-
-
             $data = [
                 'No' => $index + 1,
                 'HARI/TANGGAL' => $tanggal,
-                'RINCIAN KEGIATAN' => $Kegiatans,
-                'HASIL' => $hasil,
-
+                'RINCIAN KEGIATAN' => [],
+                'HASIL' => [],
             ];
+
+            $detailNumber = 1;
+
+            foreach ($kegiatan->detailkegiatans as $detailKegiatan) {
+                $data['RINCIAN KEGIATAN'][] = $detailNumber . '. ' . $detailKegiatan->kegiatan;
+                $data['HASIL'][] = $detailKegiatan->hasil;
+                $detailNumber++;
+            }
 
             return $data;
         });
@@ -93,61 +89,48 @@ class ExcelExport implements FromCollection, WithHeadings, ShouldAutoSize, WithS
 
         return [
             AfterSheet::class => function (AfterSheet $event) {
-                $user = auth()->user()->nama ;
-                $jabatan = auth()->user()->jabatan ;
-                $bidang = auth()->user()->bidang ;
-
-                $event->sheet->getColumnDimension('A')->setWidth(10); // Kolom A
-                $event->sheet->getColumnDimension('B')->setWidth(40); // Kolom B
-                $event->sheet->getColumnDimension('C')->setWidth(90); // Kolom C
-                $event->sheet->getColumnDimension('D')->setWidth(30); // Kolom D
+                $user = auth()->user()->nama;
+                $jabatan = auth()->user()->jabatan;
+                $bidang = auth()->user()->bidang;
 
                 // title
                 $event->sheet->mergeCells('A1:D1');
                 $event->sheet->mergeCells('A2:D2');
                 $event->sheet->mergeCells('A3:D3');
-                $event->sheet->mergeCells('C5:C6');
-                $event->sheet->mergeCells('D5:D6');
+                $event->sheet->mergeCells('A5:B5');
+                $event->sheet->mergeCells('A6:B6');
+                $event->sheet->mergeCells('C5:D6');
 
                 $event->sheet->getStyle('A1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+                $currentMonth = Carbon::now()->locale('id')->format('F');
+                $currentMonth = $this->replaceMonthInIndonesian($currentMonth);
+                $currentMonthUpper = strtoupper($currentMonth);
+                $currentYear = Carbon::now()->locale('id')->format('Y');
                 // Set value for the title (three lines)
                 $titleLines = [
                     'LAPORAN KINERJA TENAGA PENDUKUNG / TENAGA AHLI',
                     'DINAS KOMINFO PROVINSI SUMATERA UTARA',
-                    'BULAN APRIL TAHUN 2023',
+                    'BULAN '. $currentMonthUpper .' TAHUN '. $currentYear,
                 ];
                 foreach ($titleLines as $index => $titleLine) {
                     $rowIndex = $index + 1;
                     $event->sheet->setCellValue('A' . $rowIndex, $titleLine);
                 }
 
-                // Auto size all columns
-                $columns = range('A', 'D');
-                foreach ($columns as $column) {
-                    $event->sheet->getColumnDimension($column)->setAutoSize(true);
-                }
-                // end title
-
                 // header
                 // Set value for headers
                 $data = $this->collection();
-                $event->sheet->setCellValue('A5', 'NAMA');
-                $event->sheet->setCellValue('B5', $user);
-                $event->sheet->setCellValue('A6', 'JABATAN');
-                $event->sheet->setCellValue('B6', $jabatan);
-                $event->sheet->setCellValue('C5', 'BIDANG');
-                $event->sheet->setCellValue('D5', $bidang);
+                $event->sheet->setCellValue('A4', ' ' );
+                $event->sheet->setCellValue('B4', ' ');
+                $event->sheet->setCellValue('D4', ' ');
+                $event->sheet->setCellValue('C4', ' ');
+                $event->sheet->setCellValue('A5', 'NAMA : ' . $user);
+                $event->sheet->setCellValue('A6', 'JABATAN : ' . $jabatan);
+                $event->sheet->setCellValue('C5', 'BIDANG :' . $bidang);
                 $event->sheet->setCellValue('A7', 'No');
                 $event->sheet->setCellValue('B7', 'HARI/TANGGAL');
                 $event->sheet->setCellValue('C7', 'RINCIAN KEGIATAN');
                 $event->sheet->setCellValue('D7', 'HASIL');
-
-                // Auto size all columns
-                $columns = range('A', 'D'); // A sampai D karena ada 4 kolom
-                foreach ($columns as $column) {
-                    $event->sheet->getColumnDimension($column)->setAutoSize(true);
-                }
-                // end Header
 
                 // Data Kegiatan
                 $rowIndex = 8; // Mulai dari baris ke-10 (setelah header)
@@ -155,15 +138,28 @@ class ExcelExport implements FromCollection, WithHeadings, ShouldAutoSize, WithS
                 foreach ($data as $row) {
                     $event->sheet->setCellValue('A' . $rowIndex, $row['No']);
                     $event->sheet->setCellValue('B' . $rowIndex, $row['HARI/TANGGAL']);
-                    $event->sheet->setCellValue('C' . $rowIndex, $row['RINCIAN KEGIATAN']);
-                    $event->sheet->setCellValue('D' . $rowIndex, $row['HASIL']);
+
+                    // Mengisi data 'RINCIAN KEGIATAN' dan 'HASIL' ke dalam cell
+                    $rincianKegiatanCount = count($row['RINCIAN KEGIATAN']);
+                    $hasilCount = count($row['HASIL']);
+                    $maxCount = max($rincianKegiatanCount, $hasilCount);
+
+                    for ($i = 0; $i < $maxCount; $i++) {
+                        $rincian = isset($row['RINCIAN KEGIATAN'][$i]) ? $row['RINCIAN KEGIATAN'][$i] : '';
+                        $hasilItem = isset($row['HASIL'][$i]) ? $row['HASIL'][$i] : '';
+
+                        $event->sheet->setCellValue('C' . $rowIndex, $rincian);
+                        $event->sheet->setCellValue('D' . $rowIndex, $hasilItem);
+
+                        $rowIndex++;
+                    }
                     $rowIndex++;
                 }
                 // data Kegiatan
 
                 $highestRow = $event->sheet->getHighestRow();
                 $highestColumn = $event->sheet->getHighestColumn();
-                $range = 'A1:' . $highestColumn . $highestRow;
+                $range = 'A5:' . $highestColumn . $highestRow;
 
                 $event->sheet->getStyle($range)->applyFromArray([
                     'borders' => [
@@ -174,14 +170,24 @@ class ExcelExport implements FromCollection, WithHeadings, ShouldAutoSize, WithS
                     ],
                 ]);
 
-                $event->sheet->getStyle('A1:' . $highestColumn . '1')->applyFromArray([
-                    'alignment' => [
-                        'horizontal' => Alignment::HORIZONTAL_CENTER,
-                    ],
+                $event->sheet->getStyle('A5:' . $highestColumn . $highestRow)->applyFromArray([
                     'font' => [
                         'name' => 'Bookman Old Style',
+                        'size' => 12,
+                    ],
+                    'alignment' => [
+                        'horizontal' => Alignment::HORIZONTAL_CENTER,
+                        'vertical' => Alignment::VERTICAL_CENTER,
                     ],
                 ]);
+
+                $event->sheet->getStyle('A1:A3')->applyFromArray([
+                    'font' => [
+                        'name' => 'Bookman Old Style',
+                        'size' => 14,
+                    ],
+                ]);
+
 
                 $currentDate = Carbon::now()->locale('id')->format('d F Y');
                 $currentDate = $this->replaceMonthInIndonesian($currentDate);
@@ -204,14 +210,71 @@ class ExcelExport implements FromCollection, WithHeadings, ShouldAutoSize, WithS
                 $event->sheet->setCellValue($highestColumn . ($highestRow + 12), 'NIP. 19720303 199803 2 006');
 
                 $event->sheet->getStyle('B' . ($highestRow + 5))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-                $event->sheet->setCellValue('B' . ($highestRow + 5), 'Network Adminstrator Muda');
+                $event->sheet->setCellValue('B' . ($highestRow + 5), $jabatan);
 
                 $event->sheet->getStyle('B' . ($highestRow + 10))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-                $event->sheet->setCellValue('B' . ($highestRow + 10), 'Mhd Agung Prasetyo');
+                $event->sheet->setCellValue('B' . ($highestRow + 10), $user);
 
                 // Merge cells for the title
                 $highestColumn = $event->sheet->getHighestColumn();
                 $event->sheet->mergeCells('A1:' . $highestColumn . '1');
+
+                $event->sheet->getStyle('B' . ($highestRow + 5))->applyFromArray([
+                    'font' => [
+                        'name' => 'Bookman Old Style',
+                        'size' => 12,
+                    ],
+                ]);
+
+                $event->sheet->getStyle('B' . ($highestRow + 10))->applyFromArray([
+                    'font' => [
+                        'name' => 'Bookman Old Style',
+                        'size' => 12,
+                    ],
+                ]);
+
+                $event->sheet->getStyle('D' . ($highestRow + 2))->applyFromArray([
+                    'font' => [
+                        'name' => 'Bookman Old Style',
+                        'size' => 12,
+                    ],
+                ]);
+
+                $event->sheet->getStyle('D' . ($highestRow + 4))->applyFromArray([
+                    'font' => [
+                        'name' => 'Bookman Old Style',
+                        'size' => 12,
+                    ],
+                ]);
+
+                $event->sheet->getStyle('D' . ($highestRow + 5))->applyFromArray([
+                    'font' => [
+                        'name' => 'Bookman Old Style',
+                        'size' => 12,
+                    ],
+                ]);
+
+                $event->sheet->getStyle('D' . ($highestRow + 10))->applyFromArray([
+                    'font' => [
+                        'name' => 'Bookman Old Style',
+                        'size' => 12,
+                    ],
+                ]);
+
+                $event->sheet->getStyle('D' . ($highestRow + 11))->applyFromArray([
+                    'font' => [
+                        'name' => 'Bookman Old Style',
+                        'size' => 12,
+                    ],
+                ]);
+
+                $event->sheet->getStyle('D' . ($highestRow + 12))->applyFromArray([
+                    'font' => [
+                        'name' => 'Bookman Old Style',
+                        'size' => 12,
+                    ],
+                ]);
+
 
                 // // Center-align the title
                 // $titleCell = Coordinate::stringFromColumnIndex(1) . '1'; // Cell 'A1'
